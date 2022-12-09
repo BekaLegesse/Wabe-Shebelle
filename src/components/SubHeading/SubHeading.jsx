f2c3d24ea6280a81c2880039db02ee5690d6ae65
beka <bekalegesse9@gmail.com>
2022-12-09 21:40:19 +0300

    update

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Images from '../../constants/images';
+const SubHeading = ({title}) => (
+  <div style={{marginBottem: '1rem'}}>
+    <p className='p__cormorant'>{title}</p>
+    <img src={Images.spoon} alt="spoon" className='spoon__img'/>
+  </div>
+);
+
+export default SubHeading;
