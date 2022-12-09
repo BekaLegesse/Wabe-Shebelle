c170da13fbf50818a1cb92ab4b15a0c45d156956
beka <bekalegesse9@gmail.com>
2022-12-09 22:36:41 +0300

    new update

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app__wrapper_info'>
       <SubHeading title = "Chase the new flavour"/>
-      <h1 className='app__header-h1'>The key fine dining</h1>
+      <h1 className='app__header-h1'>The key fine to dining</h1>
       <p className='p__opensans' style={{margin:'2rem 0'}}>To us Hospitality means "primarily the creation of free space where the stranger can enter and become a friend instead of an enemy"</p>
       <button type='button' className='custom__button'>Explor Menu</button>
     </div>
